0b23f94408893bffef2e7dc7bec57c845f3580e1
Arianna <alh33286@uga.edu>
2022-03-13 01:14:25 -0500

    convert to functional component

--- src/pages/Teams.js
+++ src/pages/Teams.js
@@ -1,6 +1,6 @@
 import '../Teams.css';
 import TeamSelector from "../components/teamselector";
-import {Component} from "react";
+import {Component, useEffect, useMemo, useState} from "react";
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -8,78 +8,64 @@ import Col from 'react-bootstrap/Col';
 import {Link} from "react-router-dom";
 
 import siteLogo from '../static/images/basketballgenius_logo.png';
+import NavigationBar from "../components/navigationbar";
 
 
-class Teams extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { easternTeams: null };
-  }
+function Teams(props) {
 
-  getEasternTeams() {
+  const [easternTeams, updateEasternTeams] = useState(null);
+  const [teamName, updateTeamName] = useState("");
+
+  const bgImages = ["bg-kd", "bg-lebron", "bg-luka", "bg-mj", "bg-russ"]
+
+    // useMemo to set only once
+  const imgIndex = useMemo(() => {
+    return Math.floor(Math.random() * 5);
+}, [])
+
+  const getEasternTeams = () => {
       fetch("/getEasternTeams")
           .then(res => res.json())
-          .then(res => this.setState({ easternTeams: res }));
-  }
+          .then(res => updateEasternTeams(res));
+  };
 
-  componentDidMount() {
-      this.getEasternTeams();
-  }
+  useEffect(() => {
+        getEasternTeams();
+    }, []);
 
-  // render() {
-  //   return (
-  //       <div className="App">
-  //         <header className="App-header">
-  //           <img src={logo} className="App-logo" alt="logo"/>
-  //           <p>
-  //             Edit <code>src/OldApp.js</code> and save to reload.
-  //             Also, {this.state.apiResponse}.
-  //           </p>
-  //           <TeamSelector/>
-  //           <a
-  //               className="App-link"
-  //               href="https://reactjs.org"
-  //               target="_blank"
-  //               rel="noopener noreferrer"
-  //           >
-  //             Learn React
-  //           </a>
-  //         </header>
-  //       </div>
-  //   );
-  // }
-
-
-
-  render() {
 
       // No state data has been loaded yet, can't render properly
-      if (!this.state.easternTeams) {
+      if (!easternTeams) {
             return <div />
       }
 
-      const bgImages = ["bg-kd", "bg-lebron", "bg-luka", "bg-mj", "bg-russ"]
-    const imgIndex = Math.floor(Math.random() * 5);
 
+    console.log(teamName);
     return (
-        <div className={"centered " + bgImages[imgIndex] + " img-filter bg-image-dark"}>
+        <div>
+        <NavigationBar />
+        <div className={"my-5 " + bgImages[imgIndex] + " img-filter bg-image-dark"}>
+
           <Container className="justify-content-center" fluid="md">
 
-              <Row>
-                  <h1>Teams</h1>
+              <Row className="my-3">
+                  <h1 className="centered-text">Teams</h1>
               </Row>
 
                <Row className="d-grid gap-2 d-md-flex justify-content-center mb-lg-3" id="easternRows" xs={2} md={4}>
                    {/*<div className="d-grid gap-2 d-md-flex justify-content-center mb-lg-3" id="easternRows">*/}
-                   {this.state.easternTeams.map(teamObj => <Col className="grow" xs={1} md={1}><Link to={"/loading?team=" + teamObj.teamAbbrev}><TeamSelector className="centered" key={teamObj.teamName} team={teamObj}/></Link></Col>)}
+                   {easternTeams.map(teamObj => <Col className="grow" xs={1} md={1}><Link  to={"/loading?team=" + teamObj.teamAbbrev} onMouseOver={() => updateTeamName(teamObj.teamName)}><TeamSelector className="centered" key={teamObj.teamName} team={teamObj}/></Link></Col>)}
                    {/*</div>*/}
                </Row>
 
+              <Row className="my-3">
+                  <p className="centered-text first-letter">{teamName}</p>
+              </Row>
+
           </Container>
         </div>
+            </div>
     );
   }
-
-}
 
 export default Teams;
